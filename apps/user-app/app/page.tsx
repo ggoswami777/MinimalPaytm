2cb6b21d9e6bc88e975c5bd4a080150b08b3ad2d
ggoswami777 <gaurav.goswami1304@gmail.com>
2026-06-04 12:11:28 +0530

    Initial commit

---
+++ apps/user-app/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className="text-3xl font-bold text-red-500">
+      hello world
+    </div>
+  )
+}
+
+export default page
---
+++ apps/user-app/tailwind.config.js
@@ -0,0 +1,7 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "../../packages/ui/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+};
