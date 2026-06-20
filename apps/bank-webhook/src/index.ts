65438a539cf195de9ab00fc5a2e61b2ed75d711a
ggoswami777 <gaurav.goswami1304@gmail.com>
2026-06-20 21:54:53 +0530

    feat: implement bank webhook handler and next-auth credentials provider

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -1,12 +1,35 @@
 import express from "express";
 import db from "@repo/db/client"
 const app=express();
-app.post("/hdfcWebhook",(req,res)=>{
+app.post("/hdfcWebhook",async(req,res)=>{
     // TODO : Add zod validation here
+    // check if the req actually came from hdfc bank ,using a webhook secret
     const paymentInformation={
         token:req.body.token,
         userId:req.body.user_identifier,
         amount:req.body.amount
     }
     // update balance in db
+    // use transaction for both db queries
+    await db.balance.update({
+        where:{
+            userId:paymentInformation.userId
+        },
+        data:{
+            amount:{
+                increment:paymentInformation.amount
+            }
+        }
+    })
+    await db.onRampTransaction.update({
+        where:{
+            token:paymentInformation.token,
+        },
+        data:{
+            status:"Success"
+        }
+    })
+    res.status(401).json({
+        message:"captured"
+    })
 })
