b1616ced747640cc6fd8c78e6a3ea3e5a0991950
ggoswami777 <gaurav.goswami1304@gmail.com>
2026-06-24 17:40:03 +0530

    initial commit

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -1,35 +1,55 @@
 import express from "express";
-import db from "@repo/db/client"
-const app=express();
-app.post("/hdfcWebhook",async(req,res)=>{
-    // TODO : Add zod validation here
-    // check if the req actually came from hdfc bank ,using a webhook secret
-    const paymentInformation={
-        token:req.body.token,
-        userId:req.body.user_identifier,
-        amount:req.body.amount
+import db from "@repo/db/client";
+const app = express();
+
+app.use(express.json())
+
+app.post("/hdfcWebhook", async (req, res) => {
+    //TODO: Add zod validation here?
+    //TODO: HDFC bank should ideally send us a secret so we know this is sent by them
+    const paymentInformation: {
+        token: string;
+        userId: string;
+        amount: string
+    } = {
+        token: req.body.token,
+        userId: req.body.user_identifier,
+        amount: req.body.amount
+    };
+
+    try {
+        await db.$transaction([
+            db.balance.updateMany({
+                where: {
+                    userId: Number(paymentInformation.userId)
+                },
+                data: {
+                    amount: {
+                        // You can also get this from your DB
+                        increment: Number(paymentInformation.amount)
+                    }
+                }
+            }),
+            db.onRampTransaction.updateMany({
+                where: {
+                    token: paymentInformation.token
+                }, 
+                data: {
+                    status: "Success",
+                }
+            })
+        ]);
+
+        res.json({
+            message: "Captured"
+        })
+    } catch(e) {
+        console.error(e);
+        res.status(411).json({
+            message: "Error while processing webhook"
+        })
     }
-    // update balance in db
-    // use transaction for both db queries
-    await db.balance.update({
-        where:{
-            userId:paymentInformation.userId
-        },
-        data:{
-            amount:{
-                increment:paymentInformation.amount
-            }
-        }
-    })
-    await db.onRampTransaction.update({
-        where:{
-            token:paymentInformation.token,
-        },
-        data:{
-            status:"Success"
-        }
-    })
-    res.status(401).json({
-        message:"captured"
-    })
+
 })
+
+app.listen(3003);
